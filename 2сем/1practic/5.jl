39f8b99c83c358a4df3ca62aa623b6e1ea694320
ZeniTSU1447 <121384816+ZeniTSU1447@users.noreply.github.com>
2023-06-25 22:01:49 +0300

    Update 5.jl

--- 2сем/1practic/5.jl
+++ 2сем/1practic/5.jl
@@ -7,21 +7,3 @@ struct ModuloRingElement{T<:Integer}
 end
 
 import Base: +, -, *, ^, inv, -, show
-
-#Cложение
-+(a::ModuloRingElement, b::ModuloRingElement) = ModuloRingElement(a.value + b.value, a.modulus)
-
-#Вычитание
--(a::ModuloRingElement, b::ModuloRingElement) = ModuloRingElement(a.value - b.value, a.modulus)
-
-#Унарный минус
--(a::ModuloRingElement) = ModuloRingElement(-a.value, a.modulus)
-
-#Умножение
-*(a::ModuloRingElement, b::ModuloRingElement) = ModuloRingElement(a.value * b.value, a.modulus)
-
-#Обратимые элементы
-inv(a::ModuloRingElement) = ModuloRingElement(invmod(a.value, a.modulus), a.modulus)
-
-#Вывод в REPL
-show(io::IO, a::ModuloRingElement) = print(io, "$(a.value) mod $(a.modulus)")
