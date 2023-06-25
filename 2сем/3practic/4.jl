02a6f5452b54fa2ca7a50c3e6a94c1d7932c2c2e
ZeniTSU1447 <121384816+ZeniTSU1447@users.noreply.github.com>
2023-06-25 22:03:16 +0300

    Create 4.jl

---
+++ 2сем/3practic/4.jl
@@ -0,0 +1,9 @@
+function mean_mass(mass)
+    T = eltype(mass)    
+    n = 0; s1 = zero(T); s2 = zero(T)
+    for a ∈ mass
+        n += 1; s1 .+= a; s2 += a*a
+    end
+    mean = s1 ./ n
+    return mean, sqrt(s2/n - mean*mean)
+end
