a13dcfa605004ff7cf7a56c283a780aadd81c4f9
ZeniTSU1447 <121384816+ZeniTSU1447@users.noreply.github.com>
2023-06-25 22:02:56 +0300

    Create 3.jl

---
+++ 2сем/3practic/3.jl
@@ -0,0 +1,22 @@
+function factorize(n::IntType) where IntType <: Integer
+    list = NamedTuple{(:div, :deg), Tuple{IntType, IntType}}[]
+    for p in resheto(Int(ceil(n/2)))
+        k = degree(n, p) 
+        if k > 0
+            push!(list, (div=p, deg=k))
+        end
+    end
+    return list
+end
+
+function degree(n, p) 
+    k=0
+    n, r = divrem(n,p)
+    while n > 0 && r == 0
+        k += 1
+        n, r = divrem(n,p)
+    end
+    return k
+end
+
+print(factorize(420))
