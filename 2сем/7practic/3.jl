20232b7580ccc720c5a4ca9be23c119332db567f
ZeniTSU1447 <121384816+ZeniTSU1447@users.noreply.github.com>
2023-06-25 22:23:17 +0300

    Create 3.jl

---
+++ 2сем/7practic/3.jl
@@ -0,0 +1,8 @@
+# Генерация всех подмножеств
+function next_indicator!(indicator::AbstractVector{Bool})
+    i = findlast(x->(x==0), indicator)
+    isnothing(i) && return nothing
+    indicator[i] = 1
+    indicator[i+1:end] .= 0
+    return indicator 
+end
