fbb1c89f60e9637a93faba4498fc504ec2f6ddc6
ZeniTSU1447 <121384816+ZeniTSU1447@users.noreply.github.com>
2023-06-25 22:22:50 +0300

    Create 1.jl

---
+++ 2сем/7practic/1.jl
@@ -0,0 +1,8 @@
+# Генерация всех размещений с повторениями
+function next_repit_plasement!(p::Vector{T}, n::T) where T<:Integer
+    i = findlast(x->(x < n), p)
+    isnothing(i) && (return nothing)
+    p[i] += 1
+    p[i+1:end] .= 1 
+    return p
+end
